ec354a9145f4d6ce4630751281ab0bd552e37462
Nick Sweet <nckswt@gmail.com>
2020-08-19 16:36:02 -0500

    Swap options to make it even possible to run this outside of a cli

--- temp_test.sh
+++ temp_test.sh
@@ -3,7 +3,8 @@ set -ex
 # By default, using pix2pix sets norm='batch', dataset_mode='aligned', netG='unet_256', netD='basic'
 # And if is_train: pool_size=0, gan_mode='vanilla', lambda_L1=100.0
 
-# NOTE: test results (generated images from inference) will be generated under results/ (specified through --results_dir flag)
+# NOTE: test results (generated images from inference) will be generated under results/ (specified through \
+--results_dir flag)
 
 # No rooms, 1st pass, L1 = 400
 DATAROOT=mb_maps/
@@ -15,4 +16,13 @@ PHASE=test_small
 #NAME=mb_maps_pix2pix_train_2ndpass_noRooms_L1400_L1200
 #PHASE=test_small_2ndpass_noRooms_L1400
 
-python3 test.py --dataroot $DATAROOT --name $NAME --model pix2pix --gpu_ids -1 --preprocess scale_nearest256 --phase $PHASE --no_rotate --no_flip_horizontally --no_flip_vertically
+python3 test.py \
+    --dataroot $DATAROOT \
+    --name $NAME \
+    --model pix2pix \
+    --gpu_ids -1 \
+    --preprocess scale_nearest256 \
+    --phase $PHASE \
+    --no_rotate \
+    --no_flip_horizontally \
+    --no_flip_vertically
